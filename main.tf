5d8677f9fa4dfdff3a8d6cfef512ad89fca983c1
ShubhamSinghTX <130536532+ShubhamSinghTX@users.noreply.github.com>
2023-06-22 17:07:31 +0530

    Updated script

--- main.tf
+++ main.tf
@@ -1,15 +1,34 @@
 provider "aws" {
-    region = "ap-northeast-1"
+    region = "ao"
 }
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elb"
+  availability_zones = ["us-west-2b", "us-west-2c"]
 
-//s3
 
-resource "aws_s3_bucket" "abcdshubhamsingh" {
-    bucket = "abcdshubhamsingh"
-    acl = "public"
-    
-    versioning {
-        enabled = false
-    }
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
 
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = ["i-0ea9f7ea3c0127414"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
 }
